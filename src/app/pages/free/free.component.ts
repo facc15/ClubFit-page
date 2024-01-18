1e00f47ca209107d5fc6cad1f87d1a7eed6df04b
Facc <facc.15@live.com.ar>
2024-01-18 17:56:01 -0300

    add new things

--- src/app/pages/free/free.component.ts
+++ src/app/pages/free/free.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderModule } from '../../components/header/header.module';
 import { FooterModule } from '../../components/footer/footer.module';
 import { AreyoureadyComponent } from '../../components/areyouready/areyouready.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-free',
@@ -11,5 +12,32 @@ import { AreyoureadyComponent } from '../../components/areyouready/areyouready.c
   styleUrl: './free.component.css'
 })
 export class FreeComponent {
+
+  constructor(private router:Router)
+  {
+
+    
+  }
+
+  goToBlog(number:number)
+  {
+    switch(number)
+    {
+      case 1:
+        break;
+      case 2:
+        this.router.navigate(['https://drive.google.com/drive/u/0/folders/1-7SkaddqM1YVVhcDORqUfZkm-BhbCw9z']);
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+        
+
+    }
+   
+  }
 
 }
